d78506bf90fe9ab46d5ef3f06d19e4cb5532089e
Bas van Rooten <bas.vanrooten@cm.com>
2019-08-30 16:20:34 +0200

    Added project files
    Birthday calculator v1

---
+++ BirthdayCalculator/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BirthdayCalculator.Models;
+
+namespace BirthdayCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+
+            // Calculate Birthday
+            if (person.Birthday.Day == DateTime.Now.Day && person.Birthday.Month == DateTime.Now.Month)
+            {
+                // Person has its birthday today
+                return View("PositiveBirthday", person);
+            } else
+            {
+                // Person doesn't have its birthday today
+                return View("NegativeBirthday", person);
+            }
+        }
+
+        // Birthday positive view
+        public IActionResult PositiveBirthday()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BirthdayCalculator/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCalculator.Models
+{
+    public class Person
+    {
+        // Constructor
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
